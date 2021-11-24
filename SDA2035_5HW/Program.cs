7d5f793afbd32ccb0a5cdda04b67eb1d14ae27af
holonso <d1mo@live.com>
2021-11-24 23:13:27 +0900

    Добавьте файлы проекта.

---
+++ SDA2035_5HW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDA2035_5HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 7;
+            int[] array = new int[n];
+            float S = 0;
+            Console.WriteLine("Введите {0} чисел:", n);
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                S += array[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("Среднее арифметическое = {0:f2}", S / n);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ p05ex05/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p05ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите значение N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] x = new int[N, N];
+            x[0, 0] = 1;                                        //можно указать и 0 как стартовое значение массива
+            Console.Write("{0,2} ", x[0, 0]);
+            for (int j = 1; j < N; j++)                         //создание референсной строки (1 0 1 0 1..)
+            {
+                if (x[0, j - 1] == 1)
+                {
+                    x[0, j] = 0;
+                    Console.Write("{0,2} ", x[0, j]);
+                }
+                else
+                {
+                    x[0, j] = 1;
+                    Console.Write("{0,2} ", x[0, j]);
+                }
+            }
+            Console.WriteLine();
+            for (int i = 1; i < N; i++)                         //остальные строки танцуют уже от предыдущей
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (x[i - 1, j] == 1)
+                    {
+                        x[i, j] = 0;
+                    }
+                    else
+                    {
+                        x[i, j] = 1;
+                    }
+                    Console.Write("{0,2} ", x[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
